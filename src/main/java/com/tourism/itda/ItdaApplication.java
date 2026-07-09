10245c3a995c9d26c56208b86a2aa1982ad4106b
xihxxn <sihyun8870@naver.com>
2026-07-09 18:41:22 +0900

    Initial commit

---
+++ src/main/java/com/tourism/itda/ItdaApplication.java
@@ -0,0 +1,13 @@
+package com.tourism.itda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourism/itda/ItdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourism.itda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
